3e3ecd1f174ee7f818743177ffddf7c1fc1b0ac4
Madhav <55993464+madhav592001@users.noreply.github.com>
2022-02-03 11:13:41 +0530

    Update connectdb.js

--- db/connectdb.js
+++ db/connectdb.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectdb = () => {
   mongoose
     .connect(
-      process.env.MONGO_URI || "mongodb+srv://admin:buaLeU8fEMi8T5w@cluster0.ifv7x.mongodb.net/minor-project?retryWrites=true&w=majority
+      process.env.MONGO_URI || "mongodb+srv://admin:buaLeU8fEMi8T5w@cluster0.ifv7x.mongodb.net/minor-project?retryWrites=true&w=majority ,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
